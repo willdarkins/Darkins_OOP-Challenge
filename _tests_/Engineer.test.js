b6a85e2f74fb6dd676c3c220a27c41b5bee2c92c
Will Darkins <willdarkins@gmail.com>
2021-10-08 09:56:25 -0700

    engineer id number test pass

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,12 +1,18 @@
 const Engineer = require('../lib/Engineer');
 
-test('creates a engineer object', () => {
+test('creates a Engineer object', () => {
     const engineer = new Engineer();
     expect(typeof(engineer)).toBe('object');
 });
 
-test('returns name of engineer', () => {
+test('returns name of Engineer', () => {
     const name = 'Chet';
     const engineer = new Engineer(name);
     expect(engineer.getName()).toEqual(expect.any(String));
+});
+
+test('returns id number of Engineer', () => {
+    const id = 666;
+    const engineer = new Engineer('Chet', id);
+    expect(engineer.getId()).toEqual(expect.any(Number));
 });
