d7124080b88bf0ed0037600cf8974a22a149f69c
Will Darkins <willdarkins@gmail.com>
2021-10-08 10:14:40 -0700

    intern name test pass

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -4,3 +4,9 @@ test('creates a Intern object', () => {
     const intern = new Intern();
     expect(typeof(intern)).toBe('object');
 });
+
+test('returns name of Intern', () => {
+    const name = 'Billy';
+    const intern = new Intern(name);
+    expect(intern.getName()).toEqual(expect.any(String));
+});
