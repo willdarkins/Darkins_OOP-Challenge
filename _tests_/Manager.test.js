459c8112cea87431f526626031334ffe1b3818bd
Will Darkins <willdarkins@gmail.com>
2021-10-08 09:10:41 -0700

    issues with Manager Testing

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,6 +1,12 @@
 const Manager = require('../lib/Manager');
 
-test('creates an employee object', () => {
+test('creates a manager object', () => {
     const manager = new Manager();
     expect(typeof(manager)).toBe('object');
+});
+
+test('returns name of manager', () => {
+    const name = 'Dave';
+    const manager = new Manager(name);
+    expect(manager.getName()).toBe(expect.any(String));
 });
