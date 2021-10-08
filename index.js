2271a8ab5aeff5c6feb7793cdef45c2ef95245ce
Will Darkins <willdarkins@gmail.com>
2021-10-08 12:40:06 -0700

    tested Inquirer and works with sample question selecting roles

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+// const Manager = require('../lib/Manager');
+// const Engineer = require('../lib/Engineer');
+// const Intern = require('../lib/Intern');
+
 const fs = require('fs');
 const util = require("util");
 const inquirer = require('inquirer');
-const generateHTML = require('./sr')
+// const generateHTML = require('./src/generateHTML.js')
+// const writeFileAsync = util.promisify(fs.writeFile);
+
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'welcomeChoice',
+            message: 'Welcome to CLI Team Builder\n Let\'s start building your team!\n Please select one of the following members to join your team:(Required)',
+            choices: ['Manager', 'Engineer', 'Intern'],
+
+        }
+    ])
+}
+
+questions();
