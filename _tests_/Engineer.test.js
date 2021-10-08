ee43368149210bc5576b1c775d98c2d50206c408
Will Darkins <willdarkins@gmail.com>
2021-10-08 09:54:29 -0700

    engineer name test pass

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -4,3 +4,9 @@ test('creates a engineer object', () => {
     const engineer = new Engineer();
     expect(typeof(engineer)).toBe('object');
 });
+
+test('returns name of engineer', () => {
+    const name = 'Chet';
+    const engineer = new Engineer(name);
+    expect(engineer.getName()).toEqual(expect.any(String));
+});
