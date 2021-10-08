8f4d62b6e85431dd180c4d1e79d012f62ea5b100
Will Darkins <willdarkins@gmail.com>
2021-10-08 08:25:10 -0700

    Employee ID testing success

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,9 +1,18 @@
 const Employee = require('../lib/Employee');
 
 test('creates an employee object', () => {
-    const employee = new Employee ('Will');
+    const employee = new Employee();
+    expect(typeof(employee)).toBe('object');
+})
 
-    expect(employee.name).toBe('Will');
-    expect(employee.id).toEqual(expect.any(Number));
-    expect(employee.email).toEqual(expect.any(String));
+test('returns name of employee', () => {
+    const name = 'Will';
+    const employee = new Employee(name);
+    expect(employee.getName()).toBe(name);
+})
+
+test('returns id number of employee', () => {
+    const id = 98;
+    const employee = new Employee('Will', id)
+    expect(employee.getId()).toEqual(expect.any(Number))
 })
